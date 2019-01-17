eee0187b1bc2ff7170bd29f4450caad6dc9f7c1c
Andrew McDougal <andrew.mcdougal@powershop.com.au>
2019-01-17 21:03:57 +1100

    Dynamic content, remove static. Style links, nav, ewtc. Add intro text on all pages. Separate cards css.

--- templates/template-homepage.php
+++ templates/template-homepage.php
@@ -54,90 +54,39 @@ document.querySelector('.arrow-click').addEventListener('click', function () {
 						<i class="fas fa-chevron-down arrow-click"></i>
 					</div>
 				</div>
-
-				
-				
-
 				<div id="inner-content" class="wrap cf">
-
 						<main id="main" class="m-all t-3of3 d-7of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-
-								<section class="entry-content cf" itemprop="articleBody" style="display: none;">
-
-									<?php
-										// the content (pretty self explanatory huh)
-										//the_content();
-
-										/*
-										 * Link Pages is used in case you have posts that are set to break into
-										 * multiple pages. You can remove this if you don't plan on doing that.
-										 *
-										 * Also, breaking content up into multiple pages is a horrible experience,
-										 * so don't do it. While there are SOME edge cases where this is useful, it's
-										 * mostly used for people to get more ad views. It's up to you but if you want
-										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-										 *
-										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-										 *
-										*/
-										wp_link_pages( array(
-											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-											'after'       => '</div>',
-											'link_before' => '<span>',
-											'link_after'  => '</span>',
-										) );
-									?>
+								<section class="entry-content cf home-content intro-text" itemprop="articleBody">
+									<?php the_field('intro_text'); ?>
 								</section>
-
-								<!-- hardcoded for review -->
-								<section class="entry-content cf home-content" itemprop="articleBody">
-									<p class="intro-text">With 20 years experience as a musician, specialising in weddings and corporate events, Josh has performed at over 500 weddings and boasts one of the largest song repertoires in Perth. Rather than using backing tracks, his live solo performance features vocals, acoustic guitar, stomp/beat box and loop pedal, which is paired with top quality audio equipment to produce a full sound that keeps the party on the dance floor where it belongs!</p>
-
-									<!-- cards 3 column -->
+								<section class="entry-content cf" itemprop="articleBody">
 									<div class="grid-column grid-column-three">
+									<?php
+									$loop = new WP_Query( array(
+										'post_type' => 'bands',
+										'posts_per_page' => -1
+										)
+									);
+									while ( $loop->have_posts() ) : $loop->the_post(); ?>
+										<div class="card">
+											<?php if ( has_post_thumbnail() ) {the_post_thumbnail('full');} ?>
 
-
-										<div class="card card-1">
-											<img src="http://grandstandagency.com.au/website2/wp-content/uploads/2015/07/category-soloist-600x400-1-600x400.jpg" />
-											<div class="card-1-content">
-												<h3>Solo</h3>
-												<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis</p>
+											<div class="card-content">
+												<h3><?php the_title(); ?></h3>
+												<p><?php the_content(); ?></p>
 											</div>
-											<a class="button-standard"><i class="fas fa-info-circle"></i> Find out more</a>
+											<a class="button-standard" href="<?php echo get_permalink(); ?>"><i class="fas fa-info-circle"></i> Find out more</a>
 										</div>
-										<div class="card card-1">
-											<img src="http://grandstandagency.com.au/website2/wp-content/uploads/2015/07/category-duo-600x400-2-600x400.jpg" />
-											<div class="card-1-content">
-												<h3>Duo</h3>
-												<p>Ut enim ad minima veniam, quis nostrum exercitationem.</p>
-											</div>
-											<a class="button-standard"><i class="fas fa-info-circle"></i> Find out more</a>
-										</div>
-										<div class="card card-1">
-											<img src="http://grandstandagency.com.au/website2/wp-content/uploads/2018/10/wedding-entertainment-perth-hire.jpg" />
-											<div class="card-1-content">
-												<h3>Three piece</h3>
-												<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-											</div>
-											<a class="button-standard"><i class="fas fa-info-circle"></i> Find out more</a>
-										</div>
-
+										<?php endwhile; wp_reset_query(); ?>
 									</div>
-
-									<p>Depending on the size of your event, Josh can perform solo, or with his duo, trio… all the way up to a 6 piece band, which features a female singer and some of Perth’s best musicians. Josh’s accompanying musicians are all not only WAAPA trained (Western Australia Academy of Performing Arts), but have been also playing with Josh for several years, so you can expect nothing but the highest quality whichever lineup you choose.</p>
-									<p>As well as both stripped back acoustic and uptempo party music, Josh also offers a DJ service as part of all Your Wedding Singer packages and also has an interactive</p>
-									<p>Photo-booth available for hire.</p>
-									<p>Email Josh directly HERE to enquire about your special day</p>
 								</section>
-
+								<section class="entry-content cf" itemprop="articleBody">
+									<?php the_content(); ?>
+								</section>
 							</article>
-
 							<?php endwhile; else : ?>
-
 									<article id="post-not-found" class="hentry cf">
 											<header class="article-header">
 												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
@@ -149,14 +98,8 @@ document.querySelector('.arrow-click').addEventListener('click', function () {
 												<p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
 										</footer>
 									</article>
-
 							<?php endif; ?>
-
 						</main>
-
 				</div>
-
 			</div>
-
-
 <?php get_footer(); ?>
