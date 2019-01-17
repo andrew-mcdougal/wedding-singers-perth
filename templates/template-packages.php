e73fb1854439b034d09572f606281552c763db9e
Andrew McDougal <andrew.mcdougal@powershop.com.au>
2019-01-17 22:07:06 +1100

    Pages: standard, single band. Nav and subnav tweaks.

--- templates/template-packages.php
+++ templates/template-packages.php
@@ -1,16 +1,7 @@
 <?php
 /*
  Template Name: Page: Packages
- *
- * This is your custom page template. You can create as many of these as you need.
- * Simply name is "page-whatever.php" and in add the "Template Name" title at the
- * top, the same way it is here.
- *
- * When you create your page, you can just select the template and viola, you have
- * a custom page template to call your very own. Your mother would be so proud.
- *
- * For more info: http://codex.wordpress.org/Page_Templates
-*/
+ */
 ?>
 
 <?php get_header(); ?>
@@ -24,7 +15,11 @@
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 							<article class="hentry" role="article" itemscope itemtype="http://schema.org/BlogPosting">
-								<section class="entry-content cf home-content intro-text" itemprop="articleBody">
+								<header class="article-header">
+	                <h1 class="single-title custom-post-type-title"><?php the_title(); ?></h1>
+	              </header>
+              
+              	<section class="entry-content cf home-content intro-text" itemprop="articleBody">
 									<?php the_field('intro_text'); ?>
 								</section>
 								<section class="entry-content cf" itemprop="articleBody">
